e1c65799ad60e75dd47aeb6bd5f483a8e5bc1e6c
isfayhy <62905814+isfayhy@users.noreply.github.com>
2020-05-20 15:48:56 +0200

    Animation en fonction du choix
    J'ai fait en sorte de changer l'animation en fonction du bouton choisi

--- app.js
+++ app.js
@@ -6,9 +6,51 @@ gsap.from("#logo1", {duration: 1, x: 300});
 gsap.to("#logo1", {duration: 1.5, y: 350});
 
 
-//On va animer la barre du nutriscore
-var repeat = $('#barre'); 
-var tl = new TimelineMax({ repeat: 1 }) //L'animation va se répéter une fois de plus
-  .to(repeat, 1, { left: 270 }) //On anime jusqu'ou elle doit aller 
-  .to(repeat, 1, { left: 0 }) //On la fait revenir à sa position initiale
 
+
+//On va animer la barre du nutriscore en fonction du bouton choisi
+
+$('.bouton-A').on('click', function(){
+  var repeat = $('#barre'); 
+  var tl = new TimelineMax({ repeat: 0 }) //Le nombre de fois que l'animation entière va se répéter
+    .to(repeat, 1, { left: 270 }) //On anime jusqu'ou elle doit aller 
+    .to(repeat, 1, { left: 0 }) //On la fait revenir à sa position initiale
+    .to(repeat, 1, { left: 30 }) //On l'envoie à sa position finale  
+
+});
+
+$('.bouton-B').on('click', function(){
+  var repeat = $('#barre'); 
+  var tl = new TimelineMax({ repeat: 0 }) //Le nombre de fois que l'animation entière va se répéter
+    .to(repeat, 1, { left: 270 }) //On anime jusqu'ou elle doit aller 
+    .to(repeat, 1, { left: 0 }) //On la fait revenir à sa position initiale
+    .to(repeat, 1, { left: 80 }) //On l'envoie à sa position finale  
+
+});
+
+$('.bouton-C').on('click', function(){
+  var repeat = $('#barre'); 
+  var tl = new TimelineMax({ repeat: 0 }) //Le nombre de fois que l'animation entière va se répéter
+    .to(repeat, 1, { left: 270 }) //On anime jusqu'ou elle doit aller 
+    .to(repeat, 1, { left: 0 }) //On la fait revenir à sa position initiale
+    .to(repeat, 1, { left: 140 }) //On l'envoie à sa position finale  
+
+});
+
+$('.bouton-D').on('click', function(){
+  var repeat = $('#barre'); 
+  var tl = new TimelineMax({ repeat: 0 }) //Le nombre de fois que l'animation entière va se répéter
+    .to(repeat, 1, { left: 270 }) //On anime jusqu'ou elle doit aller 
+    .to(repeat, 1, { left: 0 }) //On la fait revenir à sa position initiale
+    .to(repeat, 1, { left: 190 }) //On l'envoie à sa position finale  
+
+});
+
+$('.bouton-E').on('click', function(){
+  var repeat = $('#barre'); 
+  var tl = new TimelineMax({ repeat: 0 }) //Le nombre de fois que l'animation entière va se répéter
+    .to(repeat, 1, { left: 270 }) //On anime jusqu'ou elle doit aller 
+    .to(repeat, 1, { left: 0 }) //On la fait revenir à sa position initiale
+    .to(repeat, 1, { left: 250 }) //On l'envoie à sa position finale  
+
+});
